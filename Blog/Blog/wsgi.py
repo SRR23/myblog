f989a2321ff1377209651b142f3d78af323edd9e
SRR23 <srrimon267@gmail.com>
2024-03-05 23:55:13 +0600

    added

--- Blog/Blog/wsgi.py
+++ Blog/Blog/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings')
 
 application = get_wsgi_application()
+sys.path.append('/opt/bitnami/my_project_folder/venv/lib/python3.8/site-packages')
