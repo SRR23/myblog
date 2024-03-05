84f578890d00a2fc72d75597d6b648ed79f5eb7c
SRR23 <srrimon267@gmail.com>
2024-03-05 23:07:39 +0600

    added

--- To_do_list/To_Do/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from To_Do.models import TaskModel
-# Register your models here.
-admin.site.register(TaskModel)
--- To_do_list/To_Do/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from To_Do.models import TaskModel
-
-class TaskModelForm(forms.ModelForm):
-    class Meta:
-        model = TaskModel
-        fields = ['taskTitel', 'taskDescription']
